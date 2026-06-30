ddca1b31b83461bef164568451b0cbefee2bba87
Ojus M S <ojusms@gmail.com>
2026-06-30 15:42:21 -0700

    Initial commit - Spring Boot project from Spring Initializr

---
+++ src/main/java/com/SpringBoot/JPAapp/JpAappApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.JPAapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/JPAapp/JpAappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.JPAapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
