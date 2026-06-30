7707af96bdd4f62e83e17cf89ce2fba1b5c5c525
Ojus M S <ojusms@gmail.com>
2026-06-30 15:54:48 -0700

    Add a CommandLineRunner method marked with Bean annotation in the main Application

--- src/main/java/com/SpringBoot/JPAapp/JpAappApplication.java
+++ src/main/java/com/SpringBoot/JPAapp/JpAappApplication.java
@@ -1,13 +1,22 @@
 package com.SpringBoot.JPAapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpAappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpAappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
